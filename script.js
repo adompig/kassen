154b384af2b68b59358e39ec154720d42eafaf31
adompig <141496327+adompig@users.noreply.github.com>
2023-08-05 14:18:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const groenteData = {
+let groenteData = {
   wortelen: {
     planten: "maart",
     zaaien: "april",
@@ -21,6 +21,32 @@ function updateCalendar() {
   document.getElementById("planten").innerText = `Planten in: ${plantenTime}`;
   document.getElementById("zaaien").innerText = `Zaaien in: ${zaaienTime}`;
   document.getElementById("oogsten").innerText = `Oogsten in: ${oogstenTime}`;
+}
+
+function voegGroenteToe() {
+  const nieuweGroente = document.getElementById("nieuweGroente").value;
+  const nieuwePlanten = document.getElementById("nieuwePlanten").value;
+  const nieuweZaaien = document.getElementById("nieuweZaaien").value;
+  const nieuweOogsten = document.getElementById("nieuweOogsten").value;
+
+  groenteData[nieuweGroente] = {
+    planten: nieuwePlanten,
+    zaaien: nieuweZaaien,
+    oogsten: nieuweOogsten
+  };
+
+  // Reset de invoervelden
+  document.getElementById("nieuweGroente").value = "";
+  document.getElementById("nieuwePlanten").value = "";
+  document.getElementById("nieuweZaaien").value = "";
+  document.getElementById("nieuweOogsten").value = "";
+
+  // Update de dropdown met de nieuwe groente
+  const groenteDropdown = document.getElementById("groente");
+  const nieuweGroenteOptie = document.createElement("option");
+  nieuweGroenteOptie.value = nieuweGroente;
+  nieuweGroenteOptie.textContent = nieuweGroente;
+  groenteDropdown.appendChild(nieuweGroenteOptie);
 }
 
 updateCalendar();
